12d4e07a3286a856e90702c9b9c7a4aee230e5c1
Pavitha <89039569+pavithamaha@users.noreply.github.com>
2022-05-12 23:45:54 +0530

    Add files via upload

---
+++ week2day3/Xpathcreatelead.java
@@ -0,0 +1,34 @@
+package week2day3;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.support.ui.*;
+public class Xpathcreatelead {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriverManager.firefoxdriver().setup();
+		FirefoxDriver driver=new FirefoxDriver();
+		driver.get("http://leaftaps.com/opentaps/control/main");
+		
+		//loging in
+		driver.findElement(By.xpath("//input[@id='username']")).sendKeys("demosalesmanager");
+		driver.findElement(By.xpath("//input[@id='password']")).sendKeys("crmsfa");
+		driver.findElement(By.xpath("//input[@class='decorativeSubmit']")).click();
+		driver.findElement(By.xpath("//a[contains(text(),'CRM/SFA')]")).click();
+		driver.findElement(By.xpath("//a[contains(text(),'Leads')]")).click();
+		driver.findElement(By.xpath("//a[contains(text(),'Create Lead')]")).click();
+		driver.findElement(By.xpath("//input[@id='createLeadForm_companyName']")).sendKeys("ZOHO");
+		driver.findElement(By.xpath("//input[@id='createLeadForm_firstName']")).sendKeys("pavitha");
+		driver.findElement(By.xpath("//input[@id='createLeadForm_lastName']")).sendKeys("maha");
+		
+		
+		//creating a lead
+		driver.findElement(By.xpath("//input[@class='smallSubmit']")).click();
+		driver.manage().window().maximize();
+	}
+
+}
