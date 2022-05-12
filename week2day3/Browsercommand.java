5b51509bc9fe1042f016efd383f46f0fd0c07284
Pavitha <89039569+pavithamaha@users.noreply.github.com>
2022-05-13 00:01:37 +0530

    Update Browsercommand.java

--- week2day3/Browsercommand.java
+++ week2day3/Browsercommand.java
@@ -4,7 +4,17 @@ public class Browsercommand {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+// TODO Auto-generated method stub
+		WebDriverManager.firefoxdriver().setup();
+		FirefoxDriver driver=new FirefoxDriver();
+		driver.get("http://www.facebook.com/");
+		Thread.sleep(3000);
+		driver.navigate().to("https://www.flipkart.com/");
+		Thread.sleep(3000);
+		driver.navigate().back();
+		driver.navigate().forward();
+		driver.navigate().refresh();
+		driver.close();
 	}
 
 }
